5d80cdee8bec17c32d7e21809fbf422aa6cf4ef0
AmanMahor21 <amanmahor021@gmail.com>
2024-06-10 12:35:34 +0530

    added icon

--- src/Components/WeatherCard.jsx
+++ src/Components/WeatherCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SVGWind from "../assests/icons/windsock.svg";
 import SVGHumidity from "../assests/icons/raindrop.svg";
 import SVGPresssure from "../assests/icons/thermometer.svg";
+import visiblity from "../assests/icons/Visiblity.svg";
 import DetailBox from "./DetailBox";
 import { getIconUrl, getDate } from "../helper/_functions";
 
@@ -11,52 +12,59 @@ const WeatherCard = ({ data }) => {
 
   return (
     <div className="card">
-      <div id="top">
-        <p>
-          {name}, {sys?.country}
-        </p>
-      </div>
-      <div className="upper-card">
-        <img src={getIconUrl(weatherIconCode)} alt="Is" className="img-temp" />
-        <div className="info">
-          <h2>{parseInt(main?.temp)}°C</h2>
+      <div className="card-content">
+        <div id="top">
+          <p>
+            {name}, {sys?.country}
+          </p>
+        </div>
+        <div className="upper-card">
+          <img
+            src={getIconUrl(weatherIconCode)}
+            alt="Is"
+            className="img-temp"
+          />
+          <div className="info">
+            <h2>{parseInt(main?.temp)}°C</h2>
+          </div>
+
+          <div className="info">
+            <p>{weather?.[0]?.main}</p>
+            <span>Feels like {parseInt(main?.feels_like)}</span>
+          </div>
+          <div className="info">
+            <p> {getDate("date")}</p>
+            <span>{getDate("time")}</span>
+          </div>
         </div>
 
-        <div className="info">
-          <p>{weather?.[0]?.main}</p>
-          <span>Feels like {parseInt(main?.feels_like)}</span>
+        <div className="lower-card">
+          <DetailBox
+            src={SVGHumidity}
+            title={"Humidity"}
+            width={30}
+            value={main?.humidity}
+          />
+          <DetailBox
+            src={SVGWind}
+            title={"Wind Speed"}
+            width={30}
+            value={wind?.speed}
+          />
+          <DetailBox
+            src={SVGPresssure}
+            title={"Pressure"}
+            width={30}
+            value={main?.pressure}
+          />
+          <DetailBox
+            src={visiblity}
+            // src={SVGWind}
+            title={"Visibility"}
+            width={15}
+            value={visibility}
+          />
         </div>
-        <div className="info">
-          <p> {getDate("date")}</p>
-          <span>{getDate("time")}</span>
-        </div>
-      </div>
-
-      <div className="lower-card">
-        <DetailBox
-          src={SVGHumidity}
-          title={"Humidity"}
-          width={30}
-          value={main?.humidity}
-        />
-        <DetailBox
-          src={SVGWind}
-          title={"Wind Speed"}
-          width={30}
-          value={wind?.speed}
-        />
-        <DetailBox
-          src={SVGPresssure}
-          title={"Pressure"}
-          width={30}
-          value={main?.pressure}
-        />
-        <DetailBox
-          src={SVGWind}
-          title={"Visibility"}
-          width={30}
-          value={visibility}
-        />
       </div>
     </div>
   );
